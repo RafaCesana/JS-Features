3824756c4b498c21b3fcbb6137b8a77c44ea6f07
Rafael Cesana <cesana.rafael@gmail.com>
2022-02-12 10:12:05 -0300

    Added ShowAlert function

--- MovieList/app.js
+++ MovieList/app.js
@@ -41,6 +41,29 @@ class UI{
         list.appendChild(row);
     }
 
+    static deleteMovie(target){
+        if( target.classMovie.contains('delete') ){
+            // removing the entire <tr> witch elements that contains 'delete' in <tbody>.
+            // the target witch contains the 'delete' is a <a>. The parent is <td>, and the parent again is the <tr>.
+            target.parentElement.parentElement.remove();
+        }
+    }
+
+    static showAlert(message, className){
+        const div = document.createElement('div');
+        div.className = `alert alert-${className}`;
+
+        div.appendChild(document.createTextNode(message));
+
+        const container = document.getElementById('container');
+        const form = document.getElementById('movie-form');
+        // Inserting div before form in container
+        container.insertBefore(div, form);
+
+        // Vanish in 3 seconds
+        setTimeout( () => document.querySelector('.alert').remove(), 3000 );
+    }
+
     static clearFields(){
         document.getElementById('title').value = '';
         document.getElementById('director').value = '';
@@ -64,14 +87,22 @@ document.getElementById('movie-form').addEventListener('submit', (e) => {
    const director = document.getElementById('director').value;
    const year = document.getElementById('year').value;
 
-   //Instance of Movie Class
-   const movie = new Movie(title, director, year);
+   // Validate
+   if( title === '' || director === '' || year === ''  ){
+        UI.showAlert('Please fill in all fields', 'danger');
+   } else{
+        //Instance of Movie Class
+        const movie = new Movie(title, director, year);
 
-   // Add Movie to UI
-   UI.addMovieToList(movie);
+        // Add Movie to UI
+        UI.addMovieToList(movie);
 
-   // Clear fields
-   UI.clearFields();
-} );
+        // Clear fields
+        UI.clearFields();
+   }
+});
 
 // Event: Remove a Movie
+document.getElementById('movie-list').addEventListener('click', (e) => {
+    UI.deleteMovie(e.target);
+});
