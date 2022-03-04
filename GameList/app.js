dcbeeac510737f5ba3657f61446f248721eda720
Rafael Cesana <cesana.rafael@gmail.com>
2022-03-04 16:31:10 -0300

    Class Game created
    1

---
+++ GameList/app.js
@@ -0,0 +1,8 @@
+// Game Class: Represents a Game
+class Game{
+    constructor(gameName, developerCompanyName, yearDate){
+        this.gameTitle = gameName;
+        this.developer = developerCompanyName;
+        this.year      = yearDate;
+    }
+}
